49438e2126c65622ead89497aef22cd681544180
yilwang <liyaysw@gmail.com>
2022-09-05 19:08:03 -0600

    modify test

--- cypress/e2e/2-advanced-examples/myFirstTest.cy.js
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -8,28 +8,28 @@ context('My First Test', () => {
     //cy.visit('http://example.cypress.io/commands/actions')
   })
 
-  // it('has an h1 on the page', () => {
-  //   cy.get('h1').should('exist')
-  // })
+  it('has an h1 on the page', () => {
+    cy.get('h1').should('exist')
+  })
 
-  // it('renders the corretnt h1 text', () => {
-  //   cy.get('h1').should('contain.text', 'Actions')
-  // })
+  it('renders the correct h1 text', () => {
+    cy.get('h1').should('contain.text', 'Actions')
+  })
 
-  // it('renders a paragraph under the h1', () => {
-  //   cy.get('.container').find('p').should('exist')
-  // })
+  it('renders a paragraph under the h1', () => {
+    cy.get('.container').find('p').should('exist')
+  })
 
-  // it('renders a paragraph under the h1', () => {
-  //   cy.get('.container').eq(1).find('p').should('exist')
-  // })
+  it('renders a paragraph under the h1', () => {
+    cy.get('.container').eq(1).find('p').should('exist')
+  })
 
-  // it('renders a section with an correct element', () => {
-  //   cy.get('.container').eq(2).within(() => {
-  //     cy.get('h4').should('exist')
-  //     cy.get('p').should('exist')
-  //   })
-  // })
+  it('renders a section with an correct element', () => {
+    cy.get('.container').eq(2).within(() => {
+      cy.get('h4').should('exist')
+      cy.get('p').should('exist')
+    })
+  })
 
   it('correctly renders the cypress website link', () => {
     //cy.findByText('cypress.io').should('exist')
